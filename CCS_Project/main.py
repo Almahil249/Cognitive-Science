824be24506cd57d16dc225a5a0899531769d15db
mu57f4 <mustafataha874@gmail.com>
2024-05-10 00:23:55 +0300

    Update first and second score

--- CCS_Project/main.py
+++ CCS_Project/main.py
@@ -7,15 +7,15 @@ random.seed()
 # score = 0
 # best_weight = []
 # [-0.27521385 -0.19055338 -0.54777522 -0.16039307 -0.38306999  0.35059687] -> score = 21,040
-def train(pop_size=10,iteration = 11,mutat_rate=0.1,crossover_rate=0.7):
-    output_file = 'training_output.txt'
+def train(pop_size=10,iteration = 300,mutat_rate=0.1,crossover_rate=0.7):
+    output_file = 'CCS_Project/training_output.txt'
     best_weights_file = 'best_weights_file.txt'
     POP_SIZE = pop_size
     G = GG.GA(POP_SIZE)
     currunt_score = 0
     #with open(best_weights_file, 'w') as bf:
     with open(output_file, 'w') as f:
-            for i in range(1, iteration):
+            for i in range(1, iteration+1):
                 best_gen_score = 0
                 snd_best_gen_score = 0
                 currunt_score = 0
@@ -34,13 +34,13 @@ def train(pop_size=10,iteration = 11,mutat_rate=0.1,crossover_rate=0.7):
                 #for non in G.popultion:
                 #  non_score_pop.append(non[0])
                 for chromo in G.popultion:
-                    game_score = game.main(chromo, 3000, 200000,i,weight_idnex)
+                    game_score = game.main(chromo, 5000, 200000,i,weight_idnex)
                     next_gen.append(G.fittens(chromo, game_score[2]))
                     currunt_score+=game_score[2]
                     if(best_gen_score < game_score[2]):
                         best_gen_score = game_score[2]
                         best_weight = chromo
-                    elif(snd_best_gen_score < game_score[2]):
+                    elif(snd_best_gen_score < game_score[2] and best_gen_score != game_score[2]):
                         snd_best_gen_score = game_score[2]
                         snd_best_weight = chromo
                     weight_idnex += 1
@@ -67,52 +67,6 @@ def train(pop_size=10,iteration = 11,mutat_rate=0.1,crossover_rate=0.7):
                 print(f'Second best gen Weight: {snd_best_weight}')
                 print('----------------------------------------------------------------------------')
 
-<<<<<<< HEAD
-output_file = 'training_output.txt'
-
-with open(output_file, 'w') as f:
-    for i in range(1, 301):
-        score = 0
-        currunt_score = 0
-        selected_chromo = []
-        selected_chromo_index = []
-        best_weight = []
-        next_gen = []
-        #for i2 in range(num_selected):
-         #   cromo,index = G.roluate_selection(G.popultion)
-          #  selected_chromo_index.append(index)
-           # selected_chromo.append(cromo)
-        #G.op(selected_chromo)
-        #non_score_pop = []*
-        #for non in G.popultion:
-          #  non_score_pop.append(non[0])
-        for chromo in G.popultion:
-            game_score = game.run_game_ai(chromo, 3000, 100000)
-            next_gen.append(G.fittens(chromo, game_score[2]))
-            currunt_score+=game_score[2]
-            if(score < game_score[2]):
-                score = game_score[2]
-                best_weight = chromo
-        avg_score = currunt_score//len(G.popultion)
-        #G.print_pop()
-        selected_chromo,selected_chromo_index = G.roluate_selection(next_gen)
-        print(f'chromo selcted len{selected_chromo}')
-        G.selection(selected_chromo)
-        f.write(f'=== Gen {i} Best Solution ===\n')
-        f.write(f'avg gen Score: {avg_score}')
-        f.write(f'Score: {score}\n')
-        f.write(f'Best Weight: {best_weight}\n')
-        f.write('----------------------------------------------------------------------------\n')
-        
-        print(f'=== Gen {i} Best Solution ===')
-        print(f'avg gen Score: {avg_score}')
-        print(f'Score: {score}')
-        print(f'Best Weight: {best_weight}')
-        print('----------------------------------------------------------------------------')
-
-        if score > 100000:
-            break
-=======
                 if best_gen_score > 200000:
                     #bf.write(f'=== Gen {i} Best Solution ===\n')
                     #bf.write(f'avg gen Score: {avg_score}\n')
@@ -125,4 +79,3 @@ def test(speed=3000,num_tries=600):
         game.main(best_weight, 3000, 200000,True)
 #test()
 train()
->>>>>>> 441aa806660a31e9d2232c2d988838bdcebb48d6
